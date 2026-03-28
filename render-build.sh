a9f4945483d1372f9c19f6e76b117446b2e27d7b
nsosejdi-web <nsosejdi@gmail.com>
2026-03-29 02:18:52 +0600

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
 set -e
 pip install aiogram==3.26.0 playwright==1.58.0 requests==2.33.0
-python -m playwright install
+python -c "from playwright.sync_api import sync_playwright; print('playwright ok')"
+PLAYWRIGHT_BROWSERS_PATH=/opt/render/.cache/ms-playwright python -m playwright install chromium
