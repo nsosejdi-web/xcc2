78b4519c19a8cb5885d34eecbf142d0701ef3c86
nsosejdi-web <nsosejdi@gmail.com>
2026-03-29 02:13:38 +0600

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 set -e
 pip install aiogram==3.26.0 playwright==1.58.0 requests==2.33.0
-python -m playwright install chromium
+python -m playwright install chromium-headless-shell
